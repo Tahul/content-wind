51e16c82ecc1d555793e8890225a5877626a5481
Yaël Guilloux <yael.guilloux@gmail.com>
2022-10-26 15:11:00 +0200

    feat(pinceau): add pinceau

---
+++ components/content/Box.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="as">
+    <ContentSlot :use="$slots.default" unwrap="*" />
+  </component>
+</template>
+
+<script setup lang="ts">
+import { cssProp } from 'pinceau/runtime'
+
+defineProps({
+  as: {
+    type: String,
+    required: false,
+    default: 'div'
+  },
+  css: cssProp
+})
+</script>
+
+<style lang="ts">
+css({
+  variants: {
+    padded: {}
+  }
+})
+</style>
